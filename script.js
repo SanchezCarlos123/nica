34ee190b5ce4c3ff071946f243b274269f56c893
SanchezCarlos123 <deysililiana59@gmail.com>
2022-10-08 21:53:34 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function suma(){
+var n1 = parseFloat(document.getElementById("num1").value);
+var n2 = parseFloat(document.getElementById("num2").value);
+
+var res= n1 + n2;
+document.getElementById("res").value=res;
+}
